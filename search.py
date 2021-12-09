aad179db1ef9244320c337c95269993755485edd
elgapo8765 <Jakob.Schoettl@student.uibk.ac.at>
2021-12-09 08:47:03 +0100

    update search

--- search.py
+++ search.py
@@ -2,22 +2,51 @@
 # need to be case insensitive
 # should count every substring
 # if it can't find something in the catalog it should search in dbpedia
+import pandas as pd
+
+data2 = pd.read_excel('output.xlsx', index_col=0)  
+#print(data2)
+
 
 def search(keyword, Dataframe):
     search = '|'.join(keyword)
-    searched_Des = Dataframe.loc[Dataframe['Description'].str.contains(search, na=False)].copy()
-    searched_Ab_en = Dataframe.loc[Dataframe['Abstract_en'].str.contains(search, na=False)].copy()
-    searched = searched_Des.append(searched_Ab_en)
+    search = search.lower()
+    for i in Dataframe.index:
+        try:
+            Dataframe.loc[Dataframe.index[i],'Description_search'] = Dataframe.loc[Dataframe.index[i],'Description'].lower()
+            Dataframe.loc[Dataframe.index[i],'Abstract_en_search'] = Dataframe.loc[Dataframe.index[i],'Abstract_en'].lower()
+            Dataframe.loc[Dataframe.index[i],'Abstract_de_search'] = Dataframe.loc[Dataframe.index[i],'Abstract_de'].lower()
+        except:
+            Dataframe.loc[Dataframe.index[i], 'Description_search'] = str('NaN')
+            Dataframe.loc[Dataframe.index[i],'Abstract_en_search'] = str('NaN')
+            Dataframe.loc[Dataframe.index[i],'Abstract_de_search'] = str('NaN')
+    searched_Des = Dataframe.loc[Dataframe['Description_search'].str.contains(search, na=False)].copy()
+    searched_Ab_en = Dataframe.loc[Dataframe['Abstract_en_search'].str.contains(search, na=False)].copy()
+    searched_Ab_de = Dataframe.loc[Dataframe['Abstract_de_search'].str.contains(search, na=False)].copy()
+    searched = pd.concat([searched_Des, searched_Ab_en, searched_Ab_de]).drop_duplicates().reset_index(drop=True)
     #searched = Dataframe
-    searched['sum'] = searched['Description'].str.contains(search, regex=False).astype(float) + \
-                      searched['Name'].str.contains(search, regex=False).astype(float) + \
-                      searched['IT Category'].str.contains(search, regex=False).astype(float) + \
-                      searched['ApplicationCategory'].str.contains(search, regex=False).astype(float)+ \
-                      searched['Abstract_en'].str.contains(search, regex=False).astype(float)+ \
-                      searched['Abstract_de'].str.contains(search, regex=False).astype(float)
-    ranked_search = searched.sort_values("sum", ascending=False)
+    for i in searched.index:
+        try:
+            searched.loc[searched.index[i],'Name_rank'] = searched.loc[searched.index[i],'Name'].lower()
+            searched.loc[searched.index[i],'IT Category_rank'] = searched.loc[searched.index[i],'IT Category'].lower()
+            searched.loc[searched.index[i],'ApplicationCategory_rank'] = searched.loc[searched.index[i],'ApplicationCategory'].lower()
+        except:
+            x = 1
+    for i in searched.index:
+        searched.loc[searched.index[i],'sum'] = searched.loc[searched.index[i],'Description_search'].count(search) + \
+            searched.loc[searched.index[i],'Abstract_en_search'].count(search) + \
+                searched.loc[searched.index[i],'Abstract_de_search'].count(search) +\
+                    searched.loc[searched.index[i],'Name_rank'].count(search) +\
+                        searched.loc[searched.index[i],'IT Category_rank'].count(search) +\
+                            searched.loc[searched.index[i],'ApplicationCategory_rank'].count(search)
+    try:
+        ranked_search = searched.sort_values("sum", ascending=False)
+    except:
+        print('pleas try another search')
     ranked_search = ranked_search.reset_index(inplace=False)
     out = ranked_search[['Name', 'IT Category', 'Description',  'Abstract_de', 'sum']]
-    #out.append (ranked_search['first_uri'])
-    #print(out)
     return out
+
+
+print(search(['Remote'], data2))
+
